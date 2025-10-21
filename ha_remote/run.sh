8624486582dce1c4ddbe887a892cf0b866f3b7aa
z-todorovic <107490864+z-todorovic@users.noreply.github.com>
2025-10-21 23:07:03 +0200

    Update run.sh

--- ha_remote/run.sh
+++ ha_remote/run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/with-contenv bashio
+#!/bin/sh
 export HA_REMOTE_SERVER=$(bashio::config 'server')
 export HA_REMOTE_HA_HOST=$(bashio::config 'ha_host')
 export HA_REMOTE_HA_PORT=$(bashio::config 'ha_port')
